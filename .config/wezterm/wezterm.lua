7b2f0fb0e574f7a8dbe3fb3717d0898c34ab1998
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2025-04-15 21:06:48 +0200

    Add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Catppuccin Mocha (Gogh)'
+config.font = wezterm.font('PragmataPro Mono')
+config.font_size = 15.0
+
+config.send_composed_key_when_left_alt_is_pressed = true
+
+return config
