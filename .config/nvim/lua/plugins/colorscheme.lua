b7eb36355391e6dec98013ea2b0c3a51bb3bf9a7
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2025-04-18 15:18:56 +0100

    nvim: Set colorscheme to catppuccin

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "catppuccin/nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+      opts = {
+        flavour = "mocha",
+      },
+    },
+  },
+}
