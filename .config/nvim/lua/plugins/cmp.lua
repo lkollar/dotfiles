6b60a8bfa6453f1d76d6f9992ba4e22030fac546
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2025-04-19 18:44:04 +0100

    nvim: Add 500ms debounce in blink.cmp

---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,28 @@
+return {
+    "saghen/blink.cmp",
+    opts = function(_, opts)
+        -- blink.cmp doesn't implement a debounce function. This is a workaround.
+        -- See https://github.com/Saghen/blink.cmp/issues/619.
+        local delay_ms = 500
+
+        -- disable auto_show
+        opts.completion = opts.completion or {}
+        opts.completion.menu = opts.completion.menu or {}
+        opts.completion.menu.auto_show = false
+
+        -- setup timer
+        local timer = vim.uv.new_timer()
+        vim.api.nvim_create_autocmd({ "CursorMovedI", "TextChangedI" }, {
+            callback = function()
+                timer:stop()
+                timer:start(delay_ms or 1000, 0, function()
+                    timer:stop()
+                    vim.schedule(function()
+                        -- Only run in insert mode.
+                        if vim.api.nvim_get_mode()["mode"] == "i" then require("blink.cmp").show() end
+                    end)
+                end)
+            end,
+        })
+    end,
+}
