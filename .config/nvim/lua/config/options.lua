61b2618ed5bc6f99a038eba65453249d346fe33d
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2025-04-20 15:06:02 +0100

    nvim: Set default conceal level to 0

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,7 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.snacks_animate = false
+
 vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
+vim.o.conceallevel = 0
