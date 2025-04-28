542aa20f5722fb14f53c59bb4bd6f5da261bd4f1
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2025-04-28 19:48:16 +0100

    wezterm: Use PragmataPro non-mono variant
    The mono variant doesn't always load for some reason. They seem to look
    very similar though.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Gruvbox Dark (Gogh)'
-config.font = wezterm.font('PragmataPro Mono')
+config.font = wezterm.font('PragmataPro')
 config.font_size = 15.0
 
 config.send_composed_key_when_left_alt_is_pressed = true
