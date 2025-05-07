86648ead3974cd20d1f6e1b7e7e8cd91598b6b1f
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2025-05-07 11:14:45 +0100

    wezterm: Change to JetBrains Mono font
    Use the no-ligature version.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,8 +2,8 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Gruvbox Dark (Gogh)'
-config.font = wezterm.font('PragmataPro')
-config.font_size = 15.0
+config.font = wezterm.font('JetBrains Mono NL')
+config.font_size = 14.0
 
 config.send_composed_key_when_left_alt_is_pressed = true
 
