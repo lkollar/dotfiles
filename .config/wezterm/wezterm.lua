f0027114e6c3c91951dd96b3c4ccece767819e3d
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2025-09-01 17:23:11 +0100

    Make alt-hjkl jump between tmux panes
    Using ctrl-hjkl causes conflicts with UNIX control characters: ctrl-L is
    EOF. Instead to use alt/option, create a mapping in Wezterm. We can't
    directly create a mapping in tmux, but with this solution we can capture
    and translate these keys on Wezterm's level.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,12 @@ config.color_scheme = 'Gruvbox Dark (Gogh)'
 config.font = wezterm.font('JetBrains Mono NL')
 config.font_size = 14.0
 
-config.send_composed_key_when_left_alt_is_pressed = true
+-- Map Option+hjkl to send prefix+hjkl sequences for tmux
+config.keys = {
+  { key = 'h', mods = 'ALT', action = wezterm.action.SendString('\x01h') },
+  { key = 'j', mods = 'ALT', action = wezterm.action.SendString('\x01j') },
+  { key = 'k', mods = 'ALT', action = wezterm.action.SendString('\x01k') },
+  { key = 'l', mods = 'ALT', action = wezterm.action.SendString('\x01l') },
+}
 
 return config
