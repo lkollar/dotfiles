96988106aee2a31a68f4fc0eb606304c0f017479
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2025-09-14 19:28:45 +0100

    wezterm: Remove option+hjkl tmux navigation
    If we map any keys to option/alt, wezterm will send the meta key to the
    shell, which breaks symbols which require the option key (e.g. #).
    
    This reverts f0027114e6c3c91951.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,16 +1,10 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+config.send_composed_key_when_left_alt_is_pressed = true
+
 config.color_scheme = 'Gruvbox Dark (Gogh)'
 config.font = wezterm.font('JetBrains Mono NL')
 config.font_size = 14.0
-
--- Map Option+hjkl to send prefix+hjkl sequences for tmux
-config.keys = {
-  { key = 'h', mods = 'ALT', action = wezterm.action.SendString('\x01h') },
-  { key = 'j', mods = 'ALT', action = wezterm.action.SendString('\x01j') },
-  { key = 'k', mods = 'ALT', action = wezterm.action.SendString('\x01k') },
-  { key = 'l', mods = 'ALT', action = wezterm.action.SendString('\x01l') },
-}
 
 return config
