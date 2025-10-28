51ab8457e13b4becee31c364ca813590b357b838
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2025-10-28 13:43:08 +0000

    wezterm: Add shortcuts for pane management

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,4 +7,75 @@ config.color_scheme = 'Gruvbox Dark (Gogh)'
 config.font = wezterm.font('JetBrains Mono NL')
 config.font_size = 14.0
 
+-- Vim-style pane management keybindings
+config.keys = {
+  -- Pane splitting (using Option to avoid conflicts)
+  {
+    key = 'v',
+    mods = 'OPT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 's',
+    mods = 'OPT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+
+  -- Pane navigation (vim-style hjkl)
+  {
+    key = 'h',
+    mods = 'CTRL',
+    action = wezterm.action.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'j',
+    mods = 'CTRL',
+    action = wezterm.action.ActivatePaneDirection 'Down',
+  },
+  {
+    key = 'k',
+    mods = 'CTRL',
+    action = wezterm.action.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'l',
+    mods = 'CTRL',
+    action = wezterm.action.ActivatePaneDirection 'Right',
+  },
+
+  -- Pane management
+  {
+    key = 'w',
+    mods = 'OPT',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  {
+    key = 'f',
+    mods = 'OPT',
+    action = wezterm.action.TogglePaneZoomState,
+  },
+
+  -- Pane resizing
+  {
+    key = 'h',
+    mods = 'OPT|SHIFT',
+    action = wezterm.action.AdjustPaneSize { 'Left', 2 },
+  },
+  {
+    key = 'j',
+    mods = 'OPT|SHIFT',
+    action = wezterm.action.AdjustPaneSize { 'Down', 2 },
+  },
+  {
+    key = 'k',
+    mods = 'OPT|SHIFT',
+    action = wezterm.action.AdjustPaneSize { 'Up', 2 },
+  },
+  {
+    key = 'l',
+    mods = 'OPT|SHIFT',
+    action = wezterm.action.AdjustPaneSize { 'Right', 2 },
+  },
+}
+
 return config
