8740671557c30e97e9af923aeea46a1f8c4da81a
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2026-02-13 11:03:55 +0000

    nvim: Fix neovide setup
    - Enable system clipboard use with CMD key
    - Disable animations

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -148,3 +148,21 @@ else
     },
   }
 end
+
+-- Allow clipboard copy paste in neovide
+vim.g.neovide_input_use_logo = 1
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+
+-- Disable all cursor animations
+vim.g.neovide_cursor_animation_length = 0
+vim.g.neovide_cursor_trail_size = 0
+vim.g.neovide_cursor_animate_in_insert_mode = false
+vim.g.neovide_cursor_animate_command_line = false
+
+-- Also disable position + scroll animations (optional but recommended)
+vim.g.neovide_position_animation_length = 0
+vim.g.neovide_scroll_animation_length = 0
+vim.g.neovide_scroll_animation_far_lines = 0
