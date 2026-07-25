f3698964b78899ee866093ade4b2477ea243a762
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2026-07-25 23:05:14 +0100

    nvim: Use Nix Ruff on NixOS

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,8 +1,22 @@
 return {
-  "mason-org/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "ruff",
-    },
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+
+      if vim.fn.filereadable("/etc/NIXOS") == 0 then
+        table.insert(opts.ensure_installed, "ruff")
+      end
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      if vim.fn.filereadable("/etc/NIXOS") == 1 then
+        opts.servers = opts.servers or {}
+        opts.servers.ruff = opts.servers.ruff or {}
+        opts.servers.ruff.mason = false
+      end
+    end,
   },
 }
