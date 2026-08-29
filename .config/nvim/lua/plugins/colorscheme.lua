014fa9478978b244793f72ec4c4fb7b1c598c94d
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2026-08-29 16:08:37 +0100

    nvim: Drop commented lines in colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,6 @@
 return {
     {
         'sainnhe/gruvbox-material',
-        -- lazy = false,
-        -- priority = 1000,
         config = function()
             vim.g.gruvbox_material_enable_italic = true
             vim.g.gruvbox_material_foreground = 'mix'
